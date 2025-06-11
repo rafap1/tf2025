6cfb8fbefa1d1f4ac795a2aff2570872dc5ffea6
rafap1 <rafa@portillo.eu>
2025-06-12 00:57:21 +0200

    provider with state fix

--- labs/lab_09_state/lab_09c_remote_state/1.use-state-s3-example-app-01/providers.tf
+++ labs/lab_09_state/lab_09c_remote_state/1.use-state-s3-example-app-01/providers.tf
@@ -1,19 +1,18 @@
 terraform {
   required_version = "= 1.12.1"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "= 5.99.1"
     }
   }
-
   ## Note we cannot use variables here!
 
   backend "s3" {
     bucket = "terraform-course-761528455679-state"
+    ## key refers to the s3 "path" to the state file, not to dynamodb
     ## Note key is application specific
-    key            = "mdr/example-01/terraform.tfstate" ## key refers to the s3 "path" to the state file, not to dynamodb
+    key            = "mdr/example-01/terraform.tfstate"
     dynamodb_table = "terraform-course-state-locks"
     region         = "eu-south-2"
     encrypt        = true
@@ -21,6 +20,16 @@ terraform {
   }
 }
 
+## Working backend config for student-00
+  # backend "s3" {
+  #   bucket = "terraform-course-761528455679-state"
+  #   ## Note key is application specific
+  #   key            = "mdr/example-01/terraform.tfstate" ## key refers to the s3 "path" to the state file, not to dynamodb
+  #   dynamodb_table = "terraform-course-state-locks"
+  #   region         = "eu-south-2"
+  #   encrypt        = true
+  #   profile        = "sso-student"
+  # }
 
 
 provider "aws" {
