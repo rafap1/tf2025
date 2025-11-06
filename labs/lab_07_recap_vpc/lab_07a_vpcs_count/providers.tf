56d41caa392092842e17ec3a41d07eee68042eff
rafap1 <rafa@portillo.eu>
2025-11-06 12:22:23 +0100

    proviers 1.12.x a 1.13.4

--- labs/lab_07_recap_vpc/lab_07a_vpcs_count/providers.tf
+++ labs/lab_07_recap_vpc/lab_07a_vpcs_count/providers.tf
@@ -1,12 +1,12 @@
 terraform {
   # required_version = "~> 1.12.0"
-  required_version = "1.12.1"
+  required_version = "1.13.4"
 
   required_providers {
     aws = {
       source = "hashicorp/aws"
       # version = "~>5.0"
-      version = "5.99.1"
+      version = "6.19.0"
     }
   }
 }
